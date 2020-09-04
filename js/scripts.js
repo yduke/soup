45c4f0ced376ed0d4880b2d0e8f9256f653340b5
Duke Yin <yduke@hotmail.com>
2020-09-05 02:02:34 +0800

    Offline service worker added

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ window.onload = function(){
 };
 jQuery(document).ready(function($){
 //read data output random one
-	$.post("././data.dat",null,function(data){
+	$.get("././data.dat",null,function(data){
 		var strs = new Array();
 		strs=data.split(/[(\r\n)\r\n]+/); 
 		var str = strs[Math.floor(Math.random()*strs.length)];
@@ -31,3 +31,58 @@ jQuery(document).ready(function($){
 url = window.location.href;
 $('#logo_a').attr("href",url);
  });
+
+//Service worker
+function changeModel() {
+console.log('Modle changed');
+}
+
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      window.location.reload();
+    });
+
+
+    window.addEventListener("load", function() {
+      const sw = window.navigator.serviceWorker;
+      const killSW = window.killSW || false;
+      if (!sw) {
+        return;
+      }
+
+      if (!!killSW) {
+        sw.getRegistration("./sw.js").then(registration => {
+          registration.unregister && registration.unregister();
+          window.caches &&
+            caches.keys &&
+            caches.keys().then(function(keys) {
+              keys.forEach(function(key) {
+                caches.delete(key);
+              });
+            });
+        });
+      } else {
+        sw.register("./sw.js")
+          .then(registration => {
+//            console.log("Registered events at scope: ", registration.scope);
+            if (registration.waiting) {
+              changeModel();
+              return;
+            }
+            registration.onupdatefound = function() {
+              const installingWorker = registration.installing;
+              installingWorker.onstatechange = function() {
+                switch (installingWorker.state) {
+                  case "installed":
+                    if (navigator.serviceWorker.controller) {
+                      changeModel();
+                    }
+                    break;
+                }
+              };
+            };
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      }
+    });
--- sw.js
+++ sw.js
@@ -1,56 +1,72 @@
-self.importScripts('data/games.js');
+const version = 1;
+const cacheStorageKey = "testCache-" + version;
 
-// Files to cache
-var cacheName = 'js13kPWA-v1';
-var appShellFiles = [
-  '/pwa-examples/js13kpwa/',
-  '/pwa-examples/js13kpwa/index.html',
-  '/pwa-examples/js13kpwa/app.js',
-  '/pwa-examples/js13kpwa/style.css',
-  '/pwa-examples/js13kpwa/fonts/graduate.eot',
-  '/pwa-examples/js13kpwa/fonts/graduate.ttf',
-  '/pwa-examples/js13kpwa/fonts/graduate.woff',
-  '/pwa-examples/js13kpwa/favicon.ico',
-  '/pwa-examples/js13kpwa/img/js13kgames.png',
-  '/pwa-examples/js13kpwa/img/bg.png',
-  '/pwa-examples/js13kpwa/icons/icon-32.png',
-  '/pwa-examples/js13kpwa/icons/icon-64.png',
-  '/pwa-examples/js13kpwa/icons/icon-96.png',
-  '/pwa-examples/js13kpwa/icons/icon-128.png',
-  '/pwa-examples/js13kpwa/icons/icon-168.png',
-  '/pwa-examples/js13kpwa/icons/icon-192.png',
-  '/pwa-examples/js13kpwa/icons/icon-256.png',
-  '/pwa-examples/js13kpwa/icons/icon-512.png'
+const cacheList = [
+"./", 
+"./index.html", 
+"./manifest.json", 
+"./data.dat", 
+"./css/style.css", 
+"./css/animsition.min.css", 
+"./img/logo.svg", 
+"./img/reload.svg", 
+"./img/icons/icon-512x512.png", 
+"./img/icons/icon-72x72.png", 
+"./img/icons/icon-96x96.png", 
+"./img/icons/icon-128x128.png", 
+"./img/icons/icon-144x144.png", 
+"./img/icons/icon-152x152.png", 
+"./img/icons/icon-192x192.png", 
+"./img/icons/icon-384x384.png", 
+"./js/animsition.min.js", 
+"./js/jquery.min.js", 
+"./js/scripts.js", 
 ];
-var gamesImages = [];
-for(var i=0; i<games.length; i++) {
-  gamesImages.push('data/img/'+games[i].slug+'.jpg');
-}
-var contentToCache = appShellFiles.concat(gamesImages);
 
-// Installing Service Worker
-self.addEventListener('install', function(e) {
-  console.log('[Service Worker] Install');
+self.addEventListener("install", function(e) {
+//  console.log("Cache event!");
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(contentToCache);
+    caches.open(cacheStorageKey).then(function(cache) {
+//      console.log("Adding to Cache:", cacheList);
+      return cache.addAll(cacheList);
+    })
+
+  );
+});
+
+self.addEventListener("activate", event => {
+//  console.log("Activate event");
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames => {
+        return cacheNames.filter(cacheName => cacheStorageKey !== cacheName);
+      })
+      .then(cachesToDelete => {
+        return Promise.all(
+          cachesToDelete.map(cacheToDelete => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => {
+//        console.log("Clients claims.");
+        self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
 
-// Fetching content using Service Worker
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(r) {
-      console.log('[Service Worker] Fetching resource: '+e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: ' + e.request.url);
-          cache.put(e.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+self.addEventListener("message", event => {
+  if (event.data === "skipWaiting") {
+//    console.log("Skip waiting!");
+    self.skipWaiting();
+  }
 });
